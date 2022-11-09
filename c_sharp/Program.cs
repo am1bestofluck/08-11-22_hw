769a225d13d9fed028f688e615d2adf704a71d17
am_6733 <anton6733@gmail.com>
2022-11-09 23:03:40 +0200

    t4

--- c_sharp/Program.cs
+++ c_sharp/Program.cs
@@ -4,16 +4,24 @@ using static System.Console;
 main();
 void main()
 {
+    Console.Clear();
     string[] external_todo= File.ReadLines($"../README.MD").ToArray();//получаем массив строк, строки считаются с нуля
     WriteLine(external_todo[5]);//t1
-    Array.DoubleDimensional.ArrayMultiDimensional t1 = new Array.DoubleDimensional.ArrayMultiDimensional(rows:5,columns:6);
+    ArrayMultiDimensional t1 = new ArrayMultiDimensional(rows:5,columns:3);
+    WriteLine("Первоначальный массив:");
     t1.PrintArray();
+    WriteLine();
+    WriteLine("После сортировки:");
+    t1.PrintArray();
+
     WriteLine();
     WriteLine(external_todo[16]);//t2
     WriteLine();
     WriteLine(external_todo[30]);//t3
     WriteLine();
     WriteLine(external_todo[40]);//t4
+    ArrayMultiDimensional t4= new ArrayMultiDimensional(rows:3,columns:4,layers:5);
+    t4.PrintArray();
     WriteLine();
     WriteLine(external_todo[48]);//t5
     WriteLine();
--- c_sharp/t1.cs
+++ c_sharp/t1.cs
@@ -2,76 +2,101 @@ using static System.Console;
 //DRY KISS YAGNI
 //проектирование выше соглашений
 //но когда когда пишешь, фокусируйся на форме _соглашениях_, чтобы читая фокусироваться на сути
-namespace Array.DoubleDimensional//а это точно нужно?
 //DO use PascalCasing, and separate namespace components with periods :
 // https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
+class ArrayMultiDimensional
 {
-    class ArrayMultiDimensional
-    {
-        private const int _minEstimated=0, _maxEstimated=100;
-        //underscore for private fields
-        //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/fields
-        //Pascal case for Constants
-        //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/constants?redirectedfrom=MSDN
-        //camel case for privates XD
-        //https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+    private const int _minEstimated=0, _maxEstimated=100;
+    //underscore for private fields
+    //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/fields
+    //Pascal case for Constants
+    //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/constants?redirectedfrom=MSDN
+    //camel case for privates XD
+    //https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
 
-        public int Columns,Rows;
-        public int? Layers;
-        int?[,,] _content_3d;
-        int[,] _content_2d;
-        public ArrayMultiDimensional(int rows,int columns, int? layers=null)
-        // camel case for method Parameters
-        {
-            this.Columns=columns;
-            this.Rows=rows;
-            this.Layers=layers;
-            this._content_3d=_fillArray_3d(
-                rows_i:this.Rows,
-                columns_i:this.Columns,
-                layer_i:this.Layers
-                )!;
-            this._content_2d=_fillArray(
-                rows_i:this.Rows,
-                columns_i:this.Columns);
-            
-        }
-        int [,] _fillArray(int rows_i, int columns_i)
-        {
-            //заполняем двумерный массив, рутина
-            int[,] output=new int[rows_i,columns_i];
-            return output;
-        }
-        int?[,,] _fillArray_3d(int rows_i, int columns_i, int? layer_i)
-        {
-            if (!layer_i.HasValue) return null!;
-            else
+    public int Columns,Rows;
+    public int? Layers;
+    int?[,,] _content_3d;
+    int[,] _content_2d;
+    public ArrayMultiDimensional(int rows,int columns, int? layers=null)
+    // camel case for method Parameters
+    {
+        this.Columns=columns;
+        this.Rows=rows;
+        this.Layers=layers;
+        this._content_3d=_fillArray_3d(
+            rows_i:this.Rows,
+            columns_i:this.Columns,
+            layer_i:this.Layers
+            )!;
+        this._content_2d=_fillArray(
+            rows_i:this.Rows,
+            columns_i:this.Columns);
+        
+    }
+    int [,] _fillArray(int rows_i, int columns_i)
+    {
+        //заполняем двумерный массив, рутина
+        int[,] output=new int[rows_i,columns_i];
+        Random content = new Random();
+        for (int row = 0; row < rows_i; row++)
             {
-            int?[,,] output= new int? [rows_i,columns_i,layer_i.Value];
-            int uniqueValue=new int();
-            for (int layer = 0; layer < layer_i.Value; layer++)
-            {
-                for (int row = 0; row < rows_i; row++)
+                for (int column = 0; column < columns_i; column++)
                 {
-                    for (int column = 0; column < columns_i; column++)
-                    {
-                        output[row,column,layer]=uniqueValue;
-                        uniqueValue++;
-                    }
+                    output[row,column]=content.Next(_minEstimated,_maxEstimated);
                 }
             }
-            return output;
+        return output;
+    }
+    int?[,,] _fillArray_3d(int rows_i, int columns_i, int? layer_i)
+    {
+        if (!layer_i.HasValue) return null!;
+        else
+        {
+        int?[,,] output= new int? [rows_i,columns_i,layer_i.Value];
+        int uniqueValue=10;
+        for (int layer = 0; layer < layer_i.Value; layer++)
+        {
+            for (int row = 0; row < rows_i; row++)
+            {
+                for (int column = 0; column < columns_i; column++)
+                {
+                    output[row,column,layer]=uniqueValue;
+                    uniqueValue++;
+                }
             }
         }
-        
-        public void PrintArray()
-        {
-            
+        return output;
         }
     }
-}
-
-namespace Array.TriDimensional
-{
-
+    
+    public void PrintArray()
+    {
+        if (this._content_3d!=null)
+        {
+            for (int layer = 0; layer < this._content_3d.GetLength(2); layer++)
+            {
+                for (int row = 0; row < this._content_3d.GetLength(0); row++)
+                {
+                    for (int column = 0; column < this._content_3d.GetLength(1); column++)
+                    {
+                        Write($"[{row} {column} {layer}]: {this._content_3d[row,column,layer]} ");
+                    }
+                    WriteLine();
+                }
+                WriteLine();
+            }
+        }
+        else
+        {
+            for (int row = 0; row < this._content_2d.GetLength(0); row++)
+            {
+                for (int column = 0; column < this._content_2d.GetLength(1); column++)
+                {
+                    Write($"{this._content_2d[row,column]} ");
+                }
+                WriteLine();
+            }
+        }
+    }
 }
