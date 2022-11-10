e1abcd50c1fc5ba5af64cacf2a27aa821a9d0f9d
am_6733 <anton6733@gmail.com>
2022-11-10 16:38:41 +0200

    fix later

--- c_sharp/t5.cs
+++ c_sharp/t5.cs
@@ -8,57 +8,46 @@ class SpiralShell: ArrayMultiDimensional
             int iMin=_minEstimated,
             int iMax=_maxEstimated)
     {
+
+        int[,] output= new int[iRows,iColumns];//1 cтрока
+        // int floor, int 
         int content=0;
-        int[,] output = new int[iRows,iColumns];
-            // Спиральное заполнение
-            // Задаем первое значение.
-            int coordX=0,coordY=0;
-            bool canProceed=true;
-            while (canProceed){
-            try
-            {
-                if (output[coordX+1,coordY]!=0&&
-                    output[coordX,coordY+1]!=0&&
-                    output[coordX-1,coordY]!=0&&
-                    output[coordX,coordY-1]!=0)//если шагать уже некуда
-                    {
-                        canProceed=false;//то выходим из цикла
-                    }
-                else
-                {
-                    //шагаем
-                    output[coordX,coordY]=content;//заполняем значение
-                    content++;// меняем маркер
-                    while 
-                }
-            }
-            catch (IndexOutOfRangeException e)//перехватываем очевидное
-            {
-                WriteLine($"error!: {coordX} {coordY}");//для дебага
-            }
+        for (int coordY = 0; coordY < iColumns; coordY++)
+        {
+            output[0,coordY]=content;
+            content++;
+        }
+        for (int coordX = 1; coordX < iRows; coordX++)//последний столбец
+        {
+            output[coordX,iColumns-1]=content;
+            content++;
+        }
+        for (int coordY = iColumns-1; coordY > 0; coordY--)//последняя строка
+        {
+            output[iRows-1,coordY]=content;
+            content++;
+        }
+        for (int coordX = iRows-1; coordX < 1; coordX--)//первый столбец
+        {
+            output[coordX,0]=content;
+            content++;
+        }
+        for (int coordY = 0; coordY < iColumns-1; coordY++)//вторая строка
+        {
+            output[1,coordY]=content;
+            content++;
+        }
+        for (int coordX = iRows-2; coordX < iRows-1; coordX++)//третий столбец
+        {
+            output[coordX,2]=content;
+            content++;
+        }
+        for (int coordY = iColumns-1; coordY < iColumns-2; coordY--)//третья строка
+        {
+            output[coordY,2]=content;
+            content++;
+        }
 
-            }
-
-                //шагаем по о-икс пока не край о-икс // край о-икс это или предел о-икс, или слева и справа не ноль
-                //шагаем по о-игрек пока не край о-игрек // край о-игрек это или предел о-игрек или сверху и снизу не ноль
-            }
-            // Задаем новое значение;
-            // ДВИГАЕМСЯ!
-            //если это край:
-            //    //край значит или предел по иксу или что дальше по направлению хода не ноль...дальше по направлению хода означает что значения слева и справа не нулевые
-            //проверяем верх-низ
-            //    если это край по о-игрек:
-            //    //край значит или предел по игреку или что дальше по направлению хода не ноль...дальше по направлению хода означает что значения сверху и снизу не нулевые
-            //
-            //если это НЕ край:
-            // если слева 0, а справа цифра:
-            //    прибавляем к о-икс
-            //иначе если справа 0 а слева цифра:
-            //    убавляем от о-икс
-
-
-            // МАСТЕР-КЛАСС : ВЫВЕСТИ ЭЛЕМЕНТЫ СИНХРОННО^^ 
-            // кто сказал yagni ._.
         return output;
     }
      public SpiralShell(int iRows, int iColumns): base(iRows=iRows,iColumns=iColumns)
